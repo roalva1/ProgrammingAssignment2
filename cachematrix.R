e32c37f8c42f29d5e1fa6edb641bb94ee5854690
ralonso <ralonso@MacBook-Pro-de-ralonso.local>
2014-06-07 18:15:36 +0200

    Code for the cached matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## My functions mainly are the cached vector code converted to get inversed matrix
+##
 
-## Write a short comment describing this function
+## This is the first function that create the special matrix with 4 methods:
+## set, get, setsolve and getsolve
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solved) i <<- solved
+  getsolve <- function() i
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## Here is where we calculate the inverse matrix or we get that from the cache
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+  ## Return a matrix that is the inverse of 'x'
+  i <- x$getsolve()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data)
+  x$setsolve(i)
+  i
 }
+
+mat <- matrix(data = c(4,3,3,2), nrow =2, ncol=2)
+mat_cache <- makeCacheMatrix(mat)
+# Calculating 1st inverse matrix
+mat_cache_solve <- cacheSolve(mat_cache)
+# Getting cached data
+mat_cache_solve_cached <- cacheSolve(mat_cache)
